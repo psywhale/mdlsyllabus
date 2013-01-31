c4a3c327b97801d51d407e19e6bee85b27104b60
psywhale <psycho.whale@gmail.com>
2013-01-31 13:59:59 -0600

    even more clean up

--- mod/bigbluebuttonbn/deploy.sh
+++
@@ -1,7 +0,0 @@
-clear
-sudo rm -rfv /var/www/default/htdocs/moodle22/mod/bigbluebuttonbn/*
-sudo cp -rv . /var/www/default/htdocs/moodle22/mod/bigbluebuttonbn/
-sudo chown -R www-data.www-data /var/www/default/htdocs/moodle22/mod/*
-sudo rm -rfv /var/www/default/htdocs/moodle23/mod/bigbluebuttonbn/*
-sudo cp -rv . /var/www/default/htdocs/moodle23/mod/bigbluebuttonbn/
-sudo chown -R www-data.www-data /var/www/default/htdocs/moodle23/mod/*
