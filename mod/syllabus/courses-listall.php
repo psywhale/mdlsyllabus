5d58e442703662885174d4ee37dfcaf42b543010
psywhale <psycho.whale@gmail.com>
2013-02-14 12:53:24 -0600

    got a page working for #13

--- mod/syllabus/courses-listall.php
+++ mod/syllabus/courses-listall.php
@@ -19,7 +19,7 @@ require_login($course, true, NULL);
 
 $context = get_context_instance(CONTEXT_COURSE, $course);
 
-var_dump($result);die;
+//var_dump($result);die;
 
 $PAGE->set_url('/mod/syllabus/edit.php', array('course'=>$course));
 
@@ -49,3 +49,56 @@ $PAGE->set_pagelayout('course');
 // Output starts here
 echo $OUTPUT->header();
 ?>
+
+<div class="syllabus-form-body">
+    <table class="syllabus-table"><colgroup><col /><col /><col /><col /><col /></colgroup>
+                
+                <tr class="heading"><th>Course</th><th>Title</th><th>Year</th><th>Semester</th><th>Action</th></tr>
+                <?php
+                //Get all records from course_syllabus that were created for this instance 
+                $result = $DB->get_records_sql('SELECT {course_syllabus}.id,
+                    {master_syllabus}.course_id, {master_syllabus}.course_number,
+                    {master_syllabus}.title,{course_syllabus}.instance,
+                    {course_syllabus}.year, {course_syllabus}.semester, {course_syllabus}.section_no 
+                    FROM {course_syllabus} JOIN {master_syllabus} ON 
+                    {course_syllabus}.master_syllabus_id = {master_syllabus}.id 
+                    ORDER BY 
+                    {master_syllabus}.course_number ASC');
+                // get the selected course syllabus for this instance
+                
+                
+                if(empty($result)) {
+                    echo '<tr><td colspan="4" style="text-align:center">Please <a id="add-new-course-syllabus-link" href="#">add a new</a> syllabus.</td></tr>';
+                }
+                $i = 1;
+                //var_export($result);
+                foreach($result as $key => $value) {
+                    
+                    $course_number = $value->course_number;
+                    $year = $value->year;
+                    $title = $value->title;
+                    $semester = $value->semester;
+                    $syllabusid = $value->id;
+                    $courseid = $value->course_id;
+                //    $selected = $value->selected;
+                
+                    if($i % 2 == 0) {
+                    
+                        echo '<tr class="alt"';
+                        
+                    } else {
+                        
+                        echo '<tr';
+                    }
+                    echo '><td>'.$course_number.'</td><td>'.$title.'</td><td>'.$year.'</td><td>'.$semester.'</td><td class="syllabus-center">';
+                    
+                   
+                    echo '<a href="'.$CFG->wwwroot.'/mod/syllabus/public-view.php?id='.$value->instance.'">Public Link</a>'; 
+                   
+                    echo '</td></tr>';
+                    $i++;
+                }
+                
+                ?>
+                </table>
+</div>
