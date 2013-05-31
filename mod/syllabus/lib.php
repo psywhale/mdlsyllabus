9bf80dc5ce8148a5096552484fda02a604c53039
psywhale <psycho.whale@gmail.com>
2013-05-31 11:31:28 -0500

    fixed for #32

--- mod/syllabus/lib.php
+++ mod/syllabus/lib.php
@@ -386,11 +386,11 @@ function syllabus_print($syllabus) {
         
         <strong>COURSE REQUIREMENTS</strong> $syllabus->course_requirements
         <strong>ATTENDANCE POLICY</strong> $syllabus->attendance_policy
-        <strong>GRADING POLICY</strong> $syllabus->grading_policy
+        <strong>GRADING SCALE</strong> $syllabus->grading_policy
         <strong>ACADEMIC ETHICS</strong> <br />Western Oklahoma State College is committed to instilling and upholding integrity as a core value. All members of the Western Oklahoma State College community are entrusted with academic integrity, which encompasses the fundamental values of honesty, trust, respect, fairness, and responsibility. Western is devoted to maintaining an honest academic environment and ensures fair resolution of alleged violations of academic integrity.
         <a href=\"http://wosc.edu//index.php?page=academic-integrity-policy\"> Complete Integrity Policy available here... </a> <br/><br/>
-        <strong>ASSESSMENT</strong> <br />An important part of Western’s “commitment to excellence” is the systematic collection and examination of assessment data both to document and to improve student learning. Instructors and students will be asked to participate in assessment activities as a part of the course work. The results of assessment benefit students by identifying how competently they perform course goals as well as benefiting the college by confirming Western’s performance as an institution of higher education. <br/><br/>
-        <strong>COURSE ASSESSMENT</strong> $syllabus->assessment
+        <strong>WESTERN'S ASSESSMENT POLICY</strong> <br />An important part of Western’s “commitment to excellence” is the systematic collection and examination of assessment data both to document and to improve student learning. Instructors and students will be asked to participate in assessment activities as a part of the course work. The results of assessment benefit students by identifying how competently they perform course goals as well as benefiting the college by confirming Western’s performance as an institution of higher education. <br/><br/>
+        <strong>COURSE ASSESSMENT (Evaluation)</strong> $syllabus->assessment
         <strong>ADDITIONAL INFO</strong> $syllabus->additional_info
         <strong>WITHDRAW POLICY</strong> <br/>
            A student may withdraw from one of more classes with an automatic grade of \"W\". Please read the complete withdraw policy located in <a href=\"http://www.wosc.edu/index.php?page=College-Catalog\">College Catalog</a>.
