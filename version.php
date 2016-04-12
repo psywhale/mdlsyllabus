66ba726dae9b49375584284a5cd351bab283c862
psywhale <psycho.whale@gmail.com>
2016-04-12 14:00:35 -0500

    see https://tracker.moodle.org/browse/MDL-43896

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2014081552;       // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011112900;       // Requires this Moodle version
-$module->component = 'mod_syllabus'; // Full name of the plugin (used for diagnostics)
-$module->cron      = 0;
+$plugin->version   = 2014081552;       // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011112900;       // Requires this Moodle version
+$plugin->component = 'mod_syllabus'; // Full name of the plugin (used for diagnostics)
+$plugin->cron      = 0;
